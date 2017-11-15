07c83896bde8c8ffd01bc3f1a2e125c992e736a1
felipe arriaran <felipe.arriaran@virginiogomez.cl>
2017-11-14 23:47:50 -0400

    actualizacion base de datos

--- crud1.sql
+++ crud1.sql
@@ -1,12 +1,52 @@
-create database crud1;
-use crud1;
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 15-11-2017 a las 03:47:05
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
 
-create table person(
-	id int not null auto_increment primary key,
-	name varchar(500) not null,
-	lastname varchar(500) not null,
-	address varchar(100) not null,
-	phone varchar(100) not null,
-	email varchar(255) not null,
-	created_at datetime not null
-);
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `crud1`
+--
+CREATE DATABASE `crud1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `crud1`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE IF NOT EXISTS `alumnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `seccion` varchar(25) NOT NULL,
+  `nota` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `seccion`, `nota`) VALUES
+(1, 'luis', 'regla', '1', 3.96),
+(2, 'gerson', 'chaparro', '2', 3.96),
+(3, 'chyntia', 'gonzalez', '1', 4),
+(4, 'giuliano', 'garces', '2', 4);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
