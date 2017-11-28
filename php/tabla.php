45dbeaed3f1bf507b84a199a27bc4355bb7f7dd5
felipe arriaran <felipe.arriaran@virginiogomez.cl>
2017-11-28 17:28:40 -0400

    nuevas funcionalidades

--- php/tabla.php
+++ php/tabla.php
@@ -12,28 +12,48 @@
 }
 </style>
 
-<?php
 
-include "conexion.php";
-$user_id=null;
-$sql1= "select alumnos.id,nombre,apellido,seccion,notas.nota from alumnos, notas, preguntas where alumnos.id=notas.id_alumno and preguntas.id=notas.id_pregunta order by seccion asc";
-$query = $con->query($sql1);
+<form action="ver.php" method="post">
+	<label>codigo pregunta</label> &nbsp;<input type="text" name="pregunta" value="11" disabled id="pregunta" class="sol"> <label>Considera que la Empresa Simulada en el Aula es un aporte para su desarrollo profesional?</label>
+	<div>
+		<br>
+		<label>codigo seccion&nbsp;&nbsp;&nbsp;</label> <input type="text" name="secion" id="seccion" class="sol">
+		<?php
+		include "conexion.php";
+		$user_id=null;
 
-?><form action="ver.php" method="post">
-	<label>codigo pregunta</label> &nbsp;<input type="text" name="pregunta" id="pregunta" class="sol"> <label>Considera que la Empresa Simulada en el Aula es un aporte para su desarrollo profesional?</label>
-</div>
-<div>
-	<br>
-	<label>codigo seccion</label> <input type="text" name="codigo" id="secion" class="sol"> <input type="text" name="sec" id="nom_seccion" class="luna"> 
-	<button action="ver.php" class="btn btn-primary">filtrar</button>
-</div>
+		if(!empty($_POST)){
+			$sql2= "select seccion.nombre from seccion where  seccion.id=\"$_POST[secion]\"";
+			$q = $con->query($sql2);
+			if($q->num_rows>0){
+				$d=$q->fetch_array();
+				echo '<form>';
+				echo '<input type="text" name="sec" id="nom_seccion" disabled class="luna" value="'.$d["nombre"].'">'; 
+				echo '</form>';
+			}
+		}
+		?>
+
+		<button action="ver.php" class="btn btn-primary">filtrar</button>
+	</div>
 </form>
 <br>
+<?php
+include "conexion.php";
+$user_id=null;
 
+if(!empty($_POST)){
+	$sql1= "select alumnos.id,nombre,apellido,seccion,notas.nota,id_pregunta from alumnos, notas, preguntas where id_pregunta=11 and alumnos.id=notas.id_alumno and seccion=\"$_POST[secion]\" and preguntas.id=id_pregunta order by seccion asc";
+	$query = $con->query($sql1);
+}else{
+	$sql1= "select alumnos.id,nombre,apellido,seccion,notas.nota,id_pregunta from alumnos, notas, preguntas where id_pregunta=11 and alumnos.id=notas.id_alumno and preguntas.id=id_pregunta order by seccion asc";
+	$query = $con->query($sql1);
+}
+?>
 <?php if($query->num_rows>0):?>
 	<table class="table table-bordered table-hover">
 		<thead>
-			<th class="sr-only">ID</th>
+			<th >ID</th>
 			<th>Nombre</th>
 			<th>Apellido</th>
 			<th>seccion</th>
@@ -42,7 +62,7 @@ $query = $con->query($sql1);
 		</thead>
 		<?php while ($r=$query->fetch_array()):?>
 			<tr>
-				<td  name="id" class="sr-only"><?php echo $r["id"]; ?></td>
+				<td  name="id" ><?php echo $r["id"]; ?></td>
 				<td><?php echo $r["nombre"]; ?></td>
 				<td><?php echo $r["apellido"]; ?></td>
 				<td><?php echo $r["seccion"]; ?></td>
@@ -74,29 +94,26 @@ $query = $con->query($sql1);
 	<p class="alert alert-warning">No hay resultados</p>
 <?php endif;?>
 <div>
-	<form role="form" method="post" action="ver.php"> 
-		<button id="<?php echo $r["alumno.id"];?>" class="btn btn-sm btn-success perro">Guardar
+	<form role="form" method="post" action="#"> 
+		<button class="btn btn-sm btn-success perro">Guardar
 			<?php
 
-			if(isset($_POST["nota"])){
-				if($_POST["nota"]!=""){
-					include "conexion.php";
-					$nota=array(nota);
-					print "$nota[1]";
-					for($i=0;$i<sizeof($nota);$i+=1){
 
-						$sql = "update notas set nota=\"$nota[$i]\" where notas.id_alumno=\"$_POST[id]\" and notas.id_pregunta=\"$_POST[pregunta]\ ";
-						print "$nota[$i]";
-						$query = $con->query($sql);
-					}
+			include "conexion.php";
+			if(isset($_POST["nota"])){
+				if($_POST["nota"]!=0){
+					$sql = "update notas set nota=\"$_POST[nota]\" where id_alumno=\"$_POST[id]\" and id_pregunta=11 ";
+
+					$query = $con->query($sql);
 					if($query!=null){
+
 						print "<script>alert(\"Agregado exitosamente.\");window.location='ver.php';</script>";
 					}else{
 						print "<script>alert(\"No se pudo agregar.\");window.location='ver.php';</script>";
 
 					}
 				}
-			}{}
+			}
 			?>
 		</button>
 	</form>
