f3ede7b6ceba48adb23e56d5866b31c5f07100f3
felipe arriaran <felipe.arriaran@virginiogomez.cl>
2017-11-28 17:31:02 -0400

    base de datos de prueba actualizada

--- crud1.sql
+++ crud1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 15-11-2017 a las 03:47:05
+-- Tiempo de generación: 28-11-2017 a las 21:30:18
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -33,19 +33,119 @@ CREATE TABLE IF NOT EXISTS `alumnos` (
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(40) NOT NULL,
   `seccion` varchar(25) NOT NULL,
-  `nota` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Volcado de datos para la tabla `alumnos`
 --
 
-INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `seccion`, `nota`) VALUES
-(1, 'luis', 'regla', '1', 3.96),
-(2, 'gerson', 'chaparro', '2', 3.96),
-(3, 'chyntia', 'gonzalez', '1', 4),
-(4, 'giuliano', 'garces', '2', 4);
+INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `seccion`) VALUES
+(1, 'luis', 'regla', '1'),
+(2, 'gerson', 'chaparro', '2'),
+(3, 'chyntia', 'gonzalez', '1'),
+(4, 'giuliano', 'garces', '2'),
+(5, 'felipe', 'a', '4');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE IF NOT EXISTS `notas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nota` float NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_pregunta` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Volcado de datos para la tabla `notas`
+--
+
+INSERT INTO `notas` (`id`, `nota`, `id_alumno`, `id_pregunta`) VALUES
+(1, 0, 1, 11),
+(2, 0, 2, 11),
+(3, 0, 3, 11),
+(4, 0, 4, 11),
+(5, 0, 5, 11),
+(6, 0, 5, 12),
+(8, 54, 3, 14);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE IF NOT EXISTS `preguntas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pregunta` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Volcado de datos para la tabla `preguntas`
+--
+
+INSERT INTO `preguntas` (`id`, `pregunta`) VALUES
+(1, 'pregunta1'),
+(2, 'pregunta2'),
+(3, 'pregunta3'),
+(4, 'pregunta4'),
+(5, 'pregunta5'),
+(6, 'pregunta6'),
+(7, 'pregunta7'),
+(8, 'pregunta8'),
+(9, 'pregunta9'),
+(10, 'pregunta10'),
+(11, 'pregunta11'),
+(12, 'pregunta12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seccion`
+--
+
+CREATE TABLE IF NOT EXISTS `seccion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `seccion`
+--
+
+INSERT INTO `seccion` (`id`, `nombre`) VALUES
+(1, 'contadores'),
+(2, 'auditorias'),
+(3, 'administracion'),
+(4, 'banca');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_pass`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios_pass` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(35) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `usuarios_pass`
+--
+
+INSERT INTO `usuarios_pass` (`id`, `usuario`, `password`) VALUES
+(1, 'admin', 'ipvg-2017'),
+(2, 'admin', '1234');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
